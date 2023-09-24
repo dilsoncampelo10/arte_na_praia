d33f314ca6b5a791acba7a95ea7d6bba900e5cd5
dilsoncampelo10 <dilsono316@gmail.com>
2023-09-24 12:02:18 -0300

    home

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+@section('title','Bem-Vindo')
+@section('content')
+    <div>
+        <main class="pt-5" id="container">
+            <div class="container mt-5 d-flex justify-content-center align-items-center">
+                <h1>Seja Bem-Vindo .</h1>
+            </div>
+        </main>
+        <section class="p-5">
+            <div class="container pt-4">
+                <h2 class="secondary_title">Comece a explorar</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia libero repellat facilis, totam at illum corrupti molestiae illo dolores laborum harum, maiores provident. Ratione, deserunt magnam at ex consequuntur fuga. Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque veniam nobis inventore et odit, qui laudantium, explicabo deleniti sit quas non omnis officiis perspiciatis enim mollitia dolor dicta fuga debitis.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia libero repellat facilis, totam at illum corrupti molestiae illo dolores laborum harum, maiores provident. Ratione, deserunt magnam at ex consequuntur fuga. Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque veniam nobis inventore et odit, qui laudantium, explicabo deleniti sit quas non omnis officiis perspiciatis enim mollitia dolor dicta fuga debitis.</p>
+            </div>
+        </section>
+    </div>
+@endsection
